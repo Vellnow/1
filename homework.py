e8fa11663d6e75bcdabb1a161b93a1d07d7fb90e
Vellnow <125788257+Vellnow@users.noreply.github.com>
2023-02-22 21:22:10 +0600

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,17 @@
+import datetime as dt
+
+
+class Calculator:
+    pass
+
+
+class Record:
+    pass
+
+
+class CaloriesCalculator(Calculator):
+    pass
+
+
+class CashCalculator(Calculator):
+    pass
